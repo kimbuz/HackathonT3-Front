3a8c1c932849eb078e56cab81ba52e33213cf41c
Juan Martin Bonelli <u555555@teco.com.ar>
2022-03-18 13:23:34 -0300

    Add Banner Fix

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,23 +1,48 @@
 import "./Banner.css";
 
-import { Typography, Button, withWidth, Hidden } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Typography, Button, Hidden } from "@material-ui/core";
+import { Spinner } from "../spinner/Spinner";
 
 export const Banner = () =>{
+	const [item, setItem] = useState();
+  const [url, setUrl] = useState('https://bkultrananoflow.herokuapp.com/api/v1/content/random');
+  
+  useEffect(() => {
+    const getItem = async () => {
+	    try{
+		    const response = await fetch(url);
+		    const data = await response.json();
+				console.log(data)
+		    setItem(data);
+	    } catch (error){
+	    	console.log(error)
+    	}
+	};
+		getItem();
+  }, [url] );
 
-	return(
+  return(
 		<Hidden xsDown>
-			<div className="banner-container">
-				<div className="banner">
-					<Typography variant="h2">
-						Titulo random
-					</Typography>
-					<div>
-						<Button variant="contained" color="primary">
-				         Play
-				        </Button>				
+			{ item ? (
+				<Link to={`/detail/${item._id}`}>
+					<div className="banner-container">
+						<div className="banner" style={{ backgroundImage: `url(${item.info.image_url})`}}>
+							<div className="banner-shadow"></div>
+							<div className="banner-info">
+								<Typography variant="h2">
+									{item.title}
+								</Typography>
+								<div>{item.info.plot}</div>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
+				</Link>
+			) : (
+				<Spinner />
+			)
+		};
 		</Hidden>	
-	)
+	)	
 }
